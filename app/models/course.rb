f748d467660f951df55b31d547b596b6bb78c91a
Justin Krol <justinkrol11@gmail.com>
2016-07-06 22:15:04 -0400

    Fill in model
    Include components and grades

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,24 @@
+# Course
 class Course < ActiveRecord::Base
+  [:name, :code].each { |n| validates n, presence: true, length: { minimum: 1 } }
+
+  # belongs_to :user # uncomment when user auth added
+  # belongs_to :semester # if degree-year-semester are added to the model
+  has_many :components, dependent: :destroy
+end
+
+def average
+  return nil if components.empty?
+  # return the sum of weighted averages over the total weight of all components
+  components
+    .select { |e| !e.nil? }
+    .map { :w_average } # map weighted average of each component
+    .reduce(:+) /
+    components
+    .map { |component| component[:weight] }
+    .reduce(:+)
+end
+
+def fullname
+  "#{code} - #{name}"
 end
---
+++ spec/factories/components.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :component do
+    name "MyString"
+    weight 1
+    course nil
+  end
+end
