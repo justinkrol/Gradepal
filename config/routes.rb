93bba1aea9ef1a25f84a9314084860e132388d9d
Justin Krol <justin.krol@shopify.com>
2017-01-19 20:42:43 -0500

    Root to sign in page if not signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   resources :user
   resources :courses
